a5bab4fcc9f020fb69b1a1caee0c22e62acef2d0
Joacim Odhiambo <joacim@sendyit.com>
2023-07-16 16:42:57 +0300

    Agroghala frontend - testing registration and login

---
+++ agroghala-final/src/Componets/signup.jsx
@@ -0,0 +1,247 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Login from "./../Componets/login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    phone_number: '',
+    address: '',
+    password: '',
+    confirm_password: '',
+  });
+  const [isInputActive, setInputActive] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  const handleInputChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleInputFocus = () => {
+    setInputActive(true);
+  };
+
+  const handleInputBlur = () => {
+    setInputActive(false);
+  };
+
+  const handleRegistration = async (event) => {
+    event.preventDefault();
+
+    // Reset any previous errors
+    setError(null);
+
+    // Perform validation checks
+    if (!formData.first_name) {
+      setError({ field: 'first_name', message: 'First name is required.' });
+      return;
+    }
+    if (!formData.last_name) {
+      setError({ field: 'last_name', message: 'Last name is required.' });
+      return;
+    }
+    if (!formData.email) {
+      setError({ field: 'email', message: 'Email is required.' });
+      return;
+    }
+    if (!formData.phone_number) {
+      setError({ field: 'phone_number', message: 'Phone number is required.' });
+      return;
+    }
+    if (!formData.address) {
+      setError({ field: 'address', message: 'Address is required.' });
+      return;
+    }
+    if (!formData.password) {
+      setError({ field: 'password', message: 'Password is required.' });
+      return;
+    }
+    if (formData.password !== formData.confirm_password) {
+      setError({
+        field: 'confirm_password',
+        message: 'Passwords do not match.',
+      });
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:8000', formData);
+      console.log('Sign up successful',response.data); // Handle the response as needed
+
+      // Reset the form
+      setFormData({
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone_number: '',
+        address: '',
+        password: '',
+        confirm_password: '',
+      });
+      setError(null); // Clear any previous error message
+    } catch (error) {
+      if (error.response) {
+        const { data } = error.response;
+        setError({ field: 'server', message: data.message || 'Registration failed. Please try again.' });
+      } else {
+        setError({ field: 'server', message: 'An error occurred. Please try again later.' });
+      }
+    }
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 m-5 contain">
+      <div className="card shadow-lg px-3 my-5 bg-white rounded row">
+          <div className="card-body">
+              <h2 className='mt-4'>AGROGHALA</h2>
+              <legend className='font-verdana text-center bb-2 p-3 border-bottom'>Sign up</legend>
+              <form className="d-block" onSubmit={handleRegistration}>
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="text"
+                className={`form-control ${error && error.field === 'first_name' ? 'is-invalid' : ''}`} 
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="First Name"
+                required
+              />
+              {error && error.field === 'first_name' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="text"
+                className={`form-control ${error && error.field === 'last_name' ? 'is-invalid' : ''}`}
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Last Name"
+                required
+              />
+              {error && error.field === 'last_name' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="email"
+                className={`form-control ${error && error.field === 'email' ? 'is-invalid' : ''}`}
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Email"
+                required
+              />
+              {error && error.field === 'email' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="text"
+                className={`form-control ${error && error.field === 'phone_number' ? 'is-invalid' : ''}`}
+                name="phone_number"
+                value={formData.phone_number}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Phone Number"
+                required
+              />
+              {error && error.field === 'phone_number' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="text"
+                className={`form-control ${error && error.field === 'address' ? 'is-invalid' : ''}`}
+                name="address"
+                value={formData.address}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Address"
+                required
+              />
+              {error && error.field === 'address' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group pt-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="password"
+                className={`form-control ${error && error.field === 'password' ? 'is-invalid' : ''}`}
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Password"
+                required
+              />
+              {error && error.field === 'password' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+
+            <div className={`form-group py-3 ${isInputActive ? 'has-success' : ''}`}>
+              <input
+                type="password"
+                className={`form-control ${error && error.field === 'confirm_password' ? 'is-invalid' : ''}`}
+                name="confirm_password"
+                value={formData.confirm_password}
+                onChange={handleInputChange}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                placeholder="Confirm Password"
+                required
+              />
+              {error && error.field === 'confirm_password' && <div className="invalid-feedback">{error.message}</div>}
+            </div>
+            <div className='d-flex justify-content-around my-3'>
+                <button type="submit" className="btn btn-outline-success px-5">Register</button>
+            </div>
+
+            {error && error.field === 'server' && <div className="invalid-feedback">{error.message}</div>}
+            <p className='text-muted py-3 text-center border-top'>Register with</p>
+            <div className='d-flex justify-content-around px-5 pb-3'>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="text-success bi bi-google" viewBox="0 0 16 16">
+              <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z"/>
+            </svg></a>
+            <a><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="text-success bi bi-facebook" viewBox="0 0 16 16">
+              <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+            </svg></a>
+            <a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="text-success bi bi-apple" viewBox="0 0 16 16">
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+              <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+            </svg>
+            </a>
+            <a>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="text-success bi bi-twitter" viewBox="0 0 16 16">
+              <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+            </svg>
+            </a>
+            </div>
+            <p className='text-center pb-3'>
+              Already have an account? <a className="text-success" href={<Login />}>Log in</a>
+            </p>
+            
+          </form>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationForm;
